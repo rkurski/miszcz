b01abf36515864a5e2c61ae4ff371cc07755a741
Roman Kurski <kwujciak@o2.pl>
2026-02-25 19:19:44 +0100

    Lepsze ulepszanie kukli

--- remote/features/ball/ballUpgrade.js
+++ remote/features/ball/ballUpgrade.js
@@ -1,160 +1,130 @@
-class ballUpgrade {
-  constructor() {
-      this.upgradeAllText = 'Ulepszaj wszystkie';
-      this.stopText = 'STOP';
-      this.waitingForResponse = false;
-      this.bonuses = [];
-      this.synergy = parseInt($("#ss_synergy_lvl").html());
-      this.hasStarted = false;
-      $("body").on("click", `button[data-option="ss_page"][data-page="upgrade"]`, () => {
-          this.showCheckboxes();
-          this.showUpgradeAllButton();
-      });
-      $("body").on("click", `button[data-option="ss_page"][data-page="reset"], #soulstone_interface .closeicon`, () => {
-          if (this.hasStarted) {
-              this.stopUpgrading();
-          }
-          this.hideCheckboxes();
-          this.hideUpgradeAllButton();
-      });
-      $("body").on("click", `button[data-option="ss_upgrade_all"]`, () => {
-          this.controller();
-      });
-  }
+// Ball stat upgrader - auto-upgrade with checkbox-based stat selection.
+// Accepts upgrades when sum of selected stat changes >= 0.
+(function () {
+    'use strict';
 
-  controller() {
-      if (this.hasStarted) {
-          this.hasStarted = false;
-          this.stopUpgrading();
-      } else {
-          this.hasStarted = true;
-          this.startUpgrading();
-      }
-      this.switchCheckboxesState();
-      this.switchButtonText();
-  }
+    const BALL_UPGRADE = {
+        isRunning: false,
+        bonuses: [],
 
-  startUpgrading() {
-      if (this.hasStarted) {
-          GAME.completeProgress = () => {
-              var res = GAME.progress;
-              switch (res.a) {
-                  case 45:
-                      if (res.ball) {
-                          GAME.parseData(55, res);
-                          if (this.hasStarted) {
-                              if(this.waitingForResponse) {
-                                  this.waitingForResponse = false;
-                              }
-                              this.upgrade();
-                          }
-                      }
-                      break;
-              }
-              delete GAME.progress;
-          }
-          this.bonuses = [];
-          this.upgrade();
-      }
-  }
+        async run() {
+            if (!BALL_MANAGER.acquire('upgrade')) return;
+            this.isRunning = true;
+            this._updateUI();
+            this._disableCheckboxes(true);
 
-  stopUpgrading() {
-      GAME.completeProgress = () => {
-          var res = GAME.progress;
-          switch (res.a) {
-              case 45:
-                  if (res.ball) {
-                      GAME.parseData(55, res);
-                  }
-                  break;
-          }
-          delete GAME.progress;
-      }
+            try {
+                while (this.isRunning) {
+                    // Read which stats user wants to track
+                    this._markBonuses();
+                    if (this.bonuses.length === 0 || this.bonuses.every(v => v === false)) {
+                        GAME.komunikat('[Kula] Zaznacz przynajmniej jeden stat!');
+                        break;
+                    }
 
-      this.waitingForResponse = false;
+                    // Evaluate current upgrade offer
+                    const shouldAccept = this._evaluateBonuses();
+                    if (shouldAccept) {
+                        GAME.emitOrder({ a: 45, type: 5, bid: GAME.ball_id });
+                        await this._delay(300);
+                    }
 
-      if (this.hasStarted) {
-          this.controller()
-      }
-  }
+                    // Request next upgrade
+                    GAME.emitOrder({ a: 45, type: 3, bid: GAME.ball_id });
+                    const res = await BALL_RESPONSE.waitForResponse(10000);
+                    if (!res || !this.isRunning) break;
+                }
+            } catch (e) {
+                console.warn('[BallUpgrade] Error:', e.message);
+            }
 
-  upgrade() {
-      if(this.waitingForResponse) {
-          return;
-      }
+            this.isRunning = false;
+            BALL_MANAGER.release('upgrade');
+            this._disableCheckboxes(false);
+            this._updateUI();
+        },
 
-      this.rateUpgrade();
-  }
+        stop() {
+            this.isRunning = false;
+            this._disableCheckboxes(false);
+            this._updateUI();
+        },
 
-  sendUpgrade() {
-      GAME.emitOrder({ a: 45, type: 3, bid: GAME.ball_id });
-      this.waitingForResponse = true;
-  }
+        _markBonuses() {
+            this.bonuses = [];
+            $('.ball_stats.stat_page tr[id]:not([style*="display: none"])').each((index) => {
+                const cb = $(`#bon${index + 1}_upgrade`)[0];
+                this.bonuses.push(cb ? cb.checked : false);
+            });
+        },
 
-  rateUpgrade() {
-      var shouldAcceptUpgrade = false;
-      this.markBonuses();
-      shouldAcceptUpgrade = this.evaluateBonuses();
-      if (shouldAcceptUpgrade) {
-          GAME.emitOrder({ a: 45, type: 5, bid: GAME.ball_id });
-      }
+        _evaluateBonuses() {
+            let sum = 0;
+            this.bonuses.forEach((shouldInclude, index) => {
+                if (shouldInclude) {
+                    sum += parseFloat($(`#ss_change_${index + 1}`).text()) || 0;
+                }
+            });
+            return sum >= 0;
+        },
 
-      setTimeout(this.sendUpgrade, shouldAcceptUpgrade ? 300 : 0);
-  }
+        _disableCheckboxes(disabled) {
+            $('.ball_stats.stat_page input[type=checkbox]').prop('disabled', disabled);
+        },
 
-  markBonuses() {
-      this.bonuses = [];
-      $('.ball_stats.stat_page tr[id]:not([style*="display: none"])').each((value, index, array) => {
-          this.bonuses.push($(`#bon${value + 1}_upgrade`)[0].checked);
-      }, this);
+        _updateUI() {
+            const btn = $('button[data-option="ss_upgrade_all"]');
+            btn.html(this.isRunning ? 'STOP' : 'Ulepszaj wszystkie');
+        },
 
-      let allUnchecked = this.bonuses.every((value, index, array) => {
-          value == false;
-      }, this);
+        _showCheckboxes() {
+            $('.ball_stats.stat_page tr[id]:not([style*="display: none"])').each(function (index) {
+                if (!$(`#bon${index + 1}_upgrade`).length) {
+                    $(`#stat${index + 1}_bon`).after(
+                        `<input type="checkbox" id="bon${index + 1}_upgrade" value="${index + 1}">`
+                    );
+                }
+            });
+        },
 
-      if(allUnchecked) {
-          this.stopUpgrading
-      }
-  }
+        _hideCheckboxes() {
+            $('.ball_stats.stat_page input[type=checkbox]').remove();
+        },
 
-  evaluateBonuses() {
-      var sum = 0;
-      this.bonuses.forEach((shouldInclude, index, array) => {
-          if(shouldInclude) {
-              sum += parseFloat($(`#ss_change_${index+1}`).text());
-          }
-      }, this);
+        _showButton() {
+            if (!$('button[data-option="ss_upgrade_all"]').length) {
+                $('#ss_page_upgrade > button').after(
+                    '<button class="newBtn option" data-option="ss_upgrade_all">Ulepszaj wszystkie</button>'
+                );
+            }
+        },
 
-      return sum >= 0;
-  }
+        _hideButton() {
+            $('button[data-option="ss_upgrade_all"]').remove();
+        },
 
-  showCheckboxes() {
-      $('.ball_stats.stat_page tr[id]:not([style*="display: none"])').each(function (index) {
-          $(`#stat${index + 1}_bon`).after(`<input type="checkbox" id="bon${index + 1}_upgrade" value=${index + 1}>`);
-      });
-  }
+        _delay(ms) {
+            return new Promise(r => setTimeout(r, ms));
+        }
+    };
 
-  switchCheckboxesState() {
-      $(".ball_stats.stat_page input[type=checkbox]").each((index) => {
-          $(`#bon${index + 1}_upgrade`).prop('disabled', this.hasStarted);
-      });
-  }
+    window.BALL_UPGRADE = BALL_UPGRADE;
 
-  hideCheckboxes() {
-      $(".ball_stats.stat_page input[type=checkbox]").each((index) => {
-          $(`#bon${index + 1}_upgrade`).remove();
-      });
-  }
-
-  showUpgradeAllButton() {
-      $("#ss_page_upgrade > button").after('<button class="newBtn option" data-option="ss_upgrade_all">Ulepszaj wszystkie</button>');
-  }
-
-  switchButtonText() {
-      $('#ss_page_upgrade button[data-option="ss_upgrade_all"]').html(`${this.hasStarted ? this.stopText : this.upgradeAllText}`);
-  }
-
-  hideUpgradeAllButton() {
-      $('#ss_page_upgrade button[data-option="ss_upgrade_all"]').remove();
-  }
-}
+    // UI event handlers
+    $('body').on('click', 'button[data-option="ss_page"][data-page="upgrade"]', () => {
+        BALL_UPGRADE._showCheckboxes();
+        BALL_UPGRADE._showButton();
+    });
+    $('body').on('click', 'button[data-option="ss_page"][data-page="reset"], #soulstone_interface .closeicon', () => {
+        if (BALL_UPGRADE.isRunning) BALL_UPGRADE.stop();
+        BALL_UPGRADE._hideCheckboxes();
+        BALL_UPGRADE._hideButton();
+    });
+    $('body').on('click', 'button[data-option="ss_upgrade_all"]', () => {
+        if (BALL_UPGRADE.isRunning) {
+            BALL_UPGRADE.stop();
+        } else {
+            BALL_UPGRADE.run();
+        }
+    });
+})();
